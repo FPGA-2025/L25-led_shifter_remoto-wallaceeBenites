aedb2b178e4dd954fdad028bdd784cd8e0a0ffc6
Wallace Benites <148720798+wallaceeBenites@users.noreply.github.com>
2025-07-22 00:57:08 -0300

    teste okkkk

--- led_shifter.v
+++ led_shifter.v
@@ -1,10 +1,28 @@
 module LedShifter #(
-    parameter CLK_FREQ = 25_000_000 
+    parameter CLK_FREQ = 25_000_000  
 ) (
     input  wire clk,
     input  wire rst_n,
-    output  reg [7:0] leds
+    output reg [7:0] leds
 );
 
     
+    localparam SHIFT_PERIOD = CLK_FREQ / 4;
+
+    reg [$clog2(SHIFT_PERIOD)-1:0] counter;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            leds    <= 8'b0001_1111; 
+            counter <= 0;
+        end else begin
+            if (counter == SHIFT_PERIOD - 1) begin
+                leds <= {leds[6:0], leds[7]}; 
+                counter <= 0;
+            end else begin
+                counter <= counter + 1;
+            end
+        end
+    end
+
 endmodule
